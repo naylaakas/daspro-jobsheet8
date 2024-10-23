4adaae02e4b5e677f92895c9d945b76a0dc39a0d
naylaakas <naylakas@gmail.com>
2024-10-23 08:56:05 +0700

    percobaan 4

--- RataNilai21.java
+++ RataNilai21.java
@@ -5,18 +5,8 @@ public class RataNilai21 {
         Scanner sc = new Scanner(System.in);
         int i=1, j=1, nilaiMhs;
         float nilai, totalNilai=0, rataNilai;
-        
-        for (j=1; j<=5; i++) {
             totalNilai = 0;
-            System.out.println("Input Nilai Mahasiswa ke-" +i);
-        for (j=1; j<=5; j++) {
-            System.out.print("Nilai ke-" +j + " = ");
-            nilaiMhs = sc.nextInt();
-            totalNilai+=nilaiMhs;
-        }
-            rataNilai=totalNilai/5;
-            System.out.println("Rata-rata Nilai Mahasiswa ke-" +i + " adalah " + rataNilai);
-        }
+            
         while (i<=5) {
             totalNilai=0;
             System.out.println("Input Nilai Mahasiswa ke-" +i);
